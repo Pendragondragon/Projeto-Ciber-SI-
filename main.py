7931938a436547d78a78c32b59cfb05b06c11b8d
Pedro Cardoso <pedrodamiao18@gmail.com>
2026-03-26 16:03:31 +0000

    Flask

--- main.py
+++ main.py
@@ -1,22 +1,8 @@
-from pathlib import Path
+from flask import Flask
 
-from fastapi import FastAPI
-from fastapi.responses import FileResponse
+app = Flask(__name__)
 
-app = FastAPI()
+from routes import *
 
-BASE_DIR = Path(__file__).resolve().parent
-
-@app.get("/")
-def home():
-    return "ola"
-
-
-@app.get("/login")
-def login():
-    return FileResponse(BASE_DIR / "templates" / "login.html")
-
-
-@app.get("/style.css")
-def style():
-    return FileResponse(BASE_DIR / "static" / "css" / "input.css")
+if __name__ == "__main__":
+    app.run()
