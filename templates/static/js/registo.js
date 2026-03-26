403811f659bc3384b50dfca4729e9e4cd28c19ea
Ana Silva <ana.bd.silva@ubi.pt>
2026-03-26 18:03:08 +0000

    adicionado js para registo

---
+++ templates/static/js/registo.js
@@ -0,0 +1,49 @@
+//alterar o url para estar certo
+const url = "/auth";
+
+document.getElementById('registo-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const confirmpassword = document.getElementById('confirmpassword').value;
+
+    // validacao
+    if (!username || !email || !password || !confirmpassword) {
+        alert('Please insert all the information requested.');
+        return;
+    }
+
+    if(!(password === confirmpassword)){
+        alert('Passwords dont match.');
+        return;
+    }
+
+    // iniciar pedido de registo
+    fetch(url + '/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        credentials: 'include',
+        body: JSON.stringify({ username, email, password })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            mostrarNotificacao('Successful Sign In!', 'success');
+            
+            setTimeout(() => {
+                window.location.href = "/inicio.html";
+            }, 1500);
+        } else {
+            const mensagem = data.message || data.error || 'Invalid Values.';
+            mostrarNotificacao(mensagem, 'error');
+        }
+    })
+    .catch(error => {
+        console.error('Error in login:', error);
+        alert('There was an error in login. Please try again later.');
+    });
+});
