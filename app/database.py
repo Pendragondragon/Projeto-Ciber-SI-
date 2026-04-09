ab3817d4bd4a28edab5da43f9d7c2b1a5fc2a559
Pedro Cardoso <pedrodamiao18@gmail.com>
2026-04-09 12:39:48 +0100

    Base de dados SQLlite

---
+++ app/database.py
@@ -0,0 +1,37 @@
+import sqlite3
+
+connection = sqlite3.connect("database.db")
+cursor = connection.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS user ( 
+        id INTEGER PRIMARY KEY, 
+        username TEXT, 
+        email TEXT, 
+        password TEXT
+    )
+""")
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS mensagem (
+        id INTEGER PRIMARY KEY, 
+        titulo TEXT,
+        conteudoCifrado TEXT
+    )
+""")
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS cofre (
+        id INTEGER PRIMARY KEY,
+        codigoDeAutenticacao TEXT,
+        assinaturaDigital TEXT,
+        tipoDeCifra TEXT,
+        utilizador_id INTEGER,
+        mensagem_id INTEGER,
+        FOREIGN KEY (utilizador_id) REFERENCES user(id),
+        FOREIGN KEY (mensagem_id) REFERENCES mensagem(id)
+    )
+""")
+
+connection.commit()
+connection.close()
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from main import app
 from flask import render_template
 
-@app.route("/")
+@app.route("/login")
 def login():
     return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("inicio.html")
