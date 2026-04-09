1a42e4da5750c7c9c95b6408a0a18ca1630fbfa6
Ana Silva <ana.bd.silva@ubi.pt>
2026-04-09 14:17:09 +0100

    password hash no registo

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,18 @@
 from main import app, get_db
 from flask import render_template, request, jsonify
+from flask_bcrypt import Bcrypt
 
-@app.route("/login")
+bcrypt = Bcrypt(app)
+
+@app.route("/auth/login")
 def login():
     return render_template("login.html")
 
-@app.route("/register", methods=["GET"])
+@app.route("/auth/register", methods=["GET"])
 def register_page():
     return render_template("inicio.html")
 
-@app.route("/register", methods=["POST"])
+@app.route("/auth/register", methods=["POST"])
 def register():
     data = request.get_json()
     username = data.get("username")
@@ -26,10 +29,13 @@ def register():
     if user:
         return jsonify({"success": False, "error": "Email já registado"}), 409
 
+    #password hashing 10-number of rounds for salt
+    pw_hash = bcrypt.generate_password_hash(password, 10)
+
     cursor.execute("""
         INSERT INTO user (username, email, password)
         VALUES (?, ?, ?)
-    """, (username, email, password))
+    """, (username, email, pw_hash))
     db.commit()
 
     return jsonify({"success": True, "message": "Utilizador registado com sucesso!"}), 201
