c5ee3b70a6127ec47b17f3afcaa905c24c76b0df
Ana Silva <ana.bd.silva@ubi.pt>
2026-04-23 17:59:33 +0100

    .

--- resources/static/js/new_message.js
+++ resources/static/js/new_message.js
@@ -6,12 +6,15 @@ document.addEventListener("DOMContentLoaded", async () => {
     setMethodOptionsReset();
 });
 
+//mostra só as opçoes possiveis de cada metodo
 methodSelect.addEventListener("change", () => {
     const selectedValue = methodSelect.value;
 
+    //esconde todos os que sao opcionais
     rsa_bits.style.display = "none";
     pass.style.display = "none";
 
+    //mostra so os que interessam
     if (selectedValue === "rsa") {
         rsa_bits.style.display = "block";
     } else if (selectedValue === "password") {
@@ -19,6 +22,8 @@ methodSelect.addEventListener("change", () => {
     }
 });
 
+//como a opcao default nao contem nenhuma opcional
+//como default podemos esconder tudo
 function setMethodOptionsReset(){
     rsa_bits.style.display = "none";
     pass.style.display = "none";
